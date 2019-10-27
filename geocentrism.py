8ff1a446c0d0481bfc5ba5e05448da6ef64855d8
Богданов Сергей <sergey.a.bogdanov@gazprombank.ru>
2019-10-28 01:21:32 +0300

    Проекции отделены от модели.

--- geocentrism.py
+++ geocentrism.py
@@ -27,39 +27,77 @@ class SolarSystem:
         self.au = 100;
         self.sun = Planet(7, 0, 0, "#808010", 0);
         self.mercury = Planet(2, 30, 40, "#505000", 0);
-        self.venus = Planet(4, 70, 12, "#505000", 0);
+        self.venus = Planet(4, 70, 12, "#EC5427", 0);
         self.earth = Planet(4, 100, 10, "#101080", 0);
         self.mars = Planet(3, 120, 6, "#801010", 0);
-        self.jupiter = Planet(5, 150, 1, "#505050", 0);
+        self.jupiter = Planet(5, 140, 1, "#505050", 0);
 
-    def step(self):
+    def foreach(self, f, *args, **kwargs):
         for attr in self.__dict__:
             planet = self.__dict__[attr];
             if type(planet) == Planet:
-                planet.step();
+                f(planet, *args, **kwargs);
+
+    def step(self):
+        self.foreach(Planet.step);
 
 
 class Projection:
 
-    def __init__(self):
+    def __init__(self, axes, ss):
+        self.figures = [];
+        ss.foreach(Projection.init_planet, self, axes);
+
+    @staticmethod
+    def init_planet(planet, *args, **kwargs):
+        a = list(args);
+        projection = a.pop(0);
+        args2 = tuple(a)
+        projection.init_planet_internal(planet, *args2, **kwargs);
+
+    def init_planet_internal(self, planet, *args, **kwargs):
+        fig = pyplot.Circle(self.calculate_xy(planet), planet.radius, color=planet.color);
+        self.figures.append((fig, planet));
+        args[0].add_artist(fig);
+
+    # noinspection PyMethodMayBeStatic
+    def calculate_xy(self, planet):
+        x = planet.orbit_radius * numpy.cos(planet.angular * numpy.pi / 180.);
+        y = planet.orbit_radius * numpy.sin(planet.angular * numpy.pi / 180.);
+        return x, y;
+
+    def draw(self):
+        for (fig, planet) in self.figures:
+            fig.set_center(self.calculate_xy(planet));
 
 
+def animate(value):
+    solar.step();
+    projection1.draw();
 
-def shift_solar_system(value, ss, transform_func):
-    ss.shift(10, transform_func);
 
-
-def init_rotation():
-    return solar.get_artists();
-
+solar = SolarSystem();
 
 pyplot.style.use("dark_background");
 ax = pyplot.axes(label="123");
-pyplot.axis([-160, 160, -120, 120]);
+pyplot.axis([-200, 199, -150, 149]);
 pyplot.axis(False);
 pyplot.grid(True);
+
+projection1 = Projection(ax, solar);
+
+rot_animation = animation.FuncAnimation(fig=pyplot.gcf(),
+                                        func=animate,
+                                        frames=359,
+                                        repeat=False,
+                                        interval=100);
+
+rot_animation.save("transform.gif", dpi=96, writer=matplotlib.animation.PillowWriter(96));
+
+pyplot.show();
+
+'''
 figure = pyplot.gcf();
-solar = SolarSystem();
 solar.add_to_axes(ax);
 rot_animation = animation.FuncAnimation(fig = figure,
                                         init_func = init_rotation,
@@ -72,3 +110,4 @@ rot_animation = animation.FuncAnimation(fig = figure,
 rot_animation.save("transform2.gif", dpi = 96, writer = matplotlib.animation.PillowWriter(96));
 
 pyplot.show();
+'''
