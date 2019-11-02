3100c0254836104bbffc1fef9cb81c5dba93289f
Богданов Сергей <sergey.a.bogdanov@gazprombank.ru>
2019-11-03 02:15:11 +0300

    Модель готова.
    Рисование орбит.
    Сохранение в gif.
    Устранены ошибки округления.
    Учтены краевые значения.

--- geocentrism.py
+++ geocentrism.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python
+from datetime import datetime
 
-import matplotlib;
+import matplotlib
 import matplotlib.pyplot as pyplot;
 import numpy;
 import matplotlib.animation as animation;
+
+# выводить анимацию в файл или в окно
+output_to_gif = True;
+# сколько кадров в итоговой анимации
+number_of_steps = 360;
 
 
 class Planet:
@@ -28,12 +34,23 @@ class SolarSystem:
     def __init__(self):
         self.planets = {};
         self.au = 100;
-        self.add_planet(Planet("sun", 7, 0, 0, "#808010", 0, True));
-        self.add_planet(Planet("mercury", 3, 24, 40, "#505000", 0, True));
-        self.add_planet(Planet("venus", 4, 48, 12, "#A0A0A0", 0, True));
-        self.add_planet(Planet("earth", 4, 72, 10, "#101080", 0, True));
-        self.add_planet(Planet("mars", 3, 96, 6, "#801010", 0, True));
-        self.add_planet(Planet("jupiter", 5, 120, 1, "#505050", 0, True));
+        global number_of_steps;
+        # Скорость всех планет рассчитывается от скорости Юпитера
+        # Юпитер самый медленный. Он должен за один цикл анимации сделать 1 оборот.
+        # Остальные планеты движутся со скоростями пропорционально указанным коэффициентам.
+        jupiter_step = 360 / number_of_steps;
+        traces = False;
+        self.add_planet(Planet("sun", 7, 0, jupiter_step * 0, "#909020", 0, traces and False));
+        self.add_planet(Planet("mercury", 3, 24, jupiter_step * 24, "#404000", 0, traces));
+        # self.add_planet(Planet("venus", 4, 48, jupiter_step * 12, "#A0A0A0", 0, traces and False));
+        self.add_planet(Planet("earth", 4, 72, jupiter_step * 10, "#101080", 0, traces and False));
+        # self.add_planet(Planet("mars", 3, 96, jupiter_step * 6, "#801010", 0, traces));
+        # self.add_planet(Planet("jupiter", 5, 120, jupiter_step, "#505050", 0, traces));
+        self.has_traces = False;
+        for planet in self.planets:
+            if self.planets[planet].trace:
+                self.has_traces = True;
+                break;
 
     def add_planet(self, planet):
         self.planets[planet.name] = planet;
@@ -72,8 +89,11 @@ class Projection:
     def init_planet_internal(self, planet, *args, **kwargs):
         shift_vec = self.calculate_shift_vec();
         fig = pyplot.Circle(self.calculate_xy(shift_vec, planet), planet.radius, color=planet.color);
+        fig.set_zorder(10);
         if planet.trace:
             line = pyplot.Line2D([fig.center[0]], [fig.center[1]], color=planet.color, linewidth=1);
+            line.set_zorder(1);
+            line.stop = False;
             args[0].add_artist(line);
         else:
             line = None;
@@ -91,14 +111,29 @@ class Projection:
         r = [];
         for (fig, planet, line) in self.figures:
             x, y = self.calculate_xy(shift_vec, planet);
-            if line:
-                (xdata, ydata) = line.get_data();
-                xdata.append(x);
-                ydata.append(y);
-                line.set_data((xdata, ydata));
-                r.append(line);
             fig.set_center((x, y));
             r.append(fig);
+            if line:
+                r.append(line);
+                if not line.stop:
+                    (xdata, ydata) = line.get_data();
+                    if len(xdata) > 3:
+                        dx1 = x - xdata[1];
+                        dy1 = y - ydata[1];
+                        dx2 = xdata[-1] - xdata[0];
+                        dy2 = ydata[-1] - ydata[0];
+                        dd = numpy.sqrt(numpy.abs(dx1 * dx2) + numpy.abs(dy1 * dy2));
+                        if dd < 0.00001:
+                            # если траектория замкнулась, то начало линии сохраняется в конец,
+                            # и дальнейшее рисование прекращается
+                            line.stop = True;
+                            xdata.pop();
+                            ydata.pop();
+                            x = xdata[0];
+                            y = ydata[0];
+                    xdata.append(x);
+                    ydata.append(y);
+                    line.set_data((xdata, ydata));
         return r;
 
 
@@ -138,7 +173,19 @@ class Projection3(Projection):
         return [sun_x - earth_x, sun_y - earth_y];
 
 
+def do_nothing():
+    r = [];
+    for p in [projection1, projection2, projection3]:
+        for (fig, a, b) in p.figures:
+            r.append(fig);
+    return r;
+
+
 def animate(value):
+    global number_of_steps;
+    if value == 0 or value > number_of_steps:
+        # Не двигать модель в 0-м кадре и дополнительных кадров, после последнего шага
+        return do_nothing();
     solar.step();
     r = [];
     r.extend(projection1.draw());
@@ -162,14 +209,24 @@ projection1 = Projection(ax, solar, [135, 200]);
 projection2 = Projection2(ax, solar, [425, 200]);
 projection3 = Projection3(ax, solar, [780, 200]);
 
+if output_to_gif:
+    # В gif выдается количество кадров, на 1 меньше количества шагов
+    # Последний шаг не нужен, поскольку он будет сделан в следующем цикле анимации
+    number_of_frames = number_of_steps - 1;
+else:
+    # В окне, почему-то на один кадр получается меньше
+    number_of_frames = number_of_steps + 1;
+
 rot_animation = animation.FuncAnimation(fig=pyplot.gcf(),
-                                        blit=True,
+                                        blit=False,
                                         func=animate,
-                                        frames=358,  # понятия не имею, куда делись 2 итерации
+                                        frames=number_of_frames,
                                         repeat=False,
                                         interval=100);
 
-
-rot_animation.save("transform.gif", dpi=96, writer=matplotlib.animation.PillowWriter(96));
-
-# pyplot.show();
+if output_to_gif:
+    rot_animation.save("transform-{0:%Y-%m-%d-%H-%M-%S}.gif".format(datetime.now()),
+                       dpi=96,
+                       writer=matplotlib.animation.PillowWriter(96));
+else:
+    pyplot.show();
